68ad8e0c2e7cf8b99e289cdd9ddbcdfaeeba44ca
Swathirs97 <swathyrs97@gmail.com>
2025-11-08 14:58:15 +0530

    updated

--- onlineshopping/onlineshoppingapp/urls.py
+++ onlineshopping/onlineshoppingapp/urls.py
@@ -6,10 +6,11 @@ urlpatterns = [
     path('home', views.home, name='home'),
     path('register', views.register, name='register'),
     path('list', views.productlist, name='list'),
-     path('category/<int:id>/', views.category_detail, name='category_detail'),
-     path('edit/<int:p_id>/', views.edit_product, name='edit'),
-     path('delete/<int:p_id>/', views.delete_product, name='delete'),
-     path('signup', views.signup, name='signup'),
-     path('signin', views.signin, name='signin'),
+    path('category/<int:id>/', views.category_detail, name='category_detail'),
+    path('edit/<int:p_id>/', views.edit_product, name='edit'),
+    path('delete/<int:p_id>/', views.delete_product, name='delete'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
     path('signout', views.signout, name='signout'),
+    path('search',views.search,name='search')
 ]
--- onlineshopping/onlineshoppingapp/views.py
+++ onlineshopping/onlineshoppingapp/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 # Create your views here.
 
 
@@ -47,31 +48,61 @@ def delete_product(request, p_id):
     p.delete()
     return redirect('list')
 
+# def signup(request):
+#     if request.method == 'POST':
+#         u=request.POST['username']
+#         e=request.POST['email']
+#         p=request.POST['password']
+#         f=request.POST['first_name']
+#         l=request.POST['last_name']
+#         user=User.objects.create_user(username=u,email=e,password=p,first_name=f,last_name=l)
+#         user.save()
+#     return render(request, 'signup.html')
+
+#importing and using UserCreationForm for signup
 def signup(request):
     if request.method == 'POST':
-        u=request.POST['username']
-        e=request.POST['email']
-        p=request.POST['password']
-        f=request.POST['first_name']
-        l=request.POST['last_name']
-        user=User.objects.create_user(username=u,email=e,password=p,first_name=f,last_name=l)
-        user.save()
-    return render(request, 'signup.html')
+        s = UserCreationForm(request.POST)
+        if s.is_valid():
+            s.save()
+            return redirect('signin')
+    else:
+        s = UserCreationForm()
+            
+    return render(request,'signup.html',{'form':s})
 
+# def signin(request):
+#     if request.method == 'POST':
+#         u=request.POST['username']
+#         p=request.POST['password']
+#         user=authenticate(username=u,password=p)
+#         if user is not None:
+#             login(request,user)
+#             f=user.first_name
+#             l=user.last_name
+#             return render(request, 'dashboard.html',{'fname':f,'lname':l})
+#         else:
+#             return HttpResponse("Invalid credentials")
+#     return render(request, 'signin.html')
+
+#importing and using authenticate for signin
 def signin(request):
     if request.method == 'POST':
-        u=request.POST['username']
-        p=request.POST['password']
-        user=authenticate(username=u,password=p)
-        if user is not None:
-            login(request,user)
-            f=user.first_name
-            l=user.last_name
-            return render(request, 'dashboard.html',{'fname':f,'lname':l})
-        else:
-            return HttpResponse("Invalid credentials")
-    return render(request, 'signin.html')
+        a = AuthenticationForm(request, data=request.POST)
+        if a.is_valid():
+            user = a.get_user()
+            login(request, user)
+            return render(request, 'dashboard.html')
+    else:
+        a = AuthenticationForm()
+
+    return render(request, 'signin.html', {'form': a})
 
 def signout(request):
     logout(request)
     return redirect('home')
+
+def search(request):
+    n=request.POST['n1']
+    products=Product.objects.filter(name=n)
+    return render(request,'list.html',{'products':products})
